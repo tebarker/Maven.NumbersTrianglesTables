38da870693b830b0538ac8734664b16b27fb9439
Tenae <tenae@Zipcoders-MacBook-Pro-107.local>
2022-06-27 09:33:47 -0400

    tables complete

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,29 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        int tableSize = 5;
+        return getMultiplicationTable(tableSize);
+
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        int tableSize = 10;
+        return getMultiplicationTable(tableSize);
+
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+
+        StringBuilder output = new StringBuilder();
+        for(int width = 1; width <= tableSize; width++) {
+            for(int height = 1; height <= tableSize; height++) {
+                output.append(String.format("%3d", width * height));
+                output.append(" |");
+            }
+            output.append("\n");
+        }
+        String table = output.toString();
+        return table;
+
     }
 }
